6d3463549738e567fc16466802baa42d2bf82395
lacunag <luis.acuna2008@gmail.com>
2022-07-08 15:33:44 -0400

    initial

---
+++ application/controllers/User.php
@@ -0,0 +1,87 @@
+<?php
+
+// example controller code
+
+class Users extends CI_Controller {
+
+
+
+
+    public function users()
+    {
+        $users = $this->user_model->get_users();
+        $this->load->view('user', array('users' => $users));
+    }
+
+    public function user($id)
+    {
+        $user = $this->user_model->get_user($id);
+        $this->load->view('user', array('user' => $user));
+    }
+
+    public function add_user()
+    {
+        // validation input name, email, phone, picture file
+        $this->form_validation->set_rules('name', 'Name', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required');
+        $this->form_validation->set_rules('phone', 'Phone', 'required');
+        $this->form_validation->set_rules('picture', 'Picture', 'required');
+        // validation input name, email, phone, picture
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('user_add');
+        } else {
+            $name = $this->input->post('name');
+            $email = $this->input->post('email');
+            $phone = $this->input->post('phone');
+            $picture = $this->input->post('picture');
+
+            $resp = $this->user_model->add_user($name, $email, $phone, $picture);
+            if ($resp) {
+                redirect('test/users');
+            } else {
+                $this->load->view('user_add');
+            }
+                         
+        }
+    }
+
+    public function edit_user($id)
+    {
+        // validation input name, email, phone, picture
+        $this->form_validation->set_rules('name', 'Name', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required');
+        $this->form_validation->set_rules('phone', 'Phone', 'required');
+        $this->form_validation->set_rules('picture', 'Picture', 'required');
+        // validation input name, email, phone, picture
+
+        if ($this->form_validation->run() == FALSE) {
+            $user = $this->user_model->get_user($id);
+            $this->load->view('user_edit', array('user' => $user));
+        } else {
+            $name = $this->input->post('name');
+            $email = $this->input->post('email');
+            $phone = $this->input->post('phone');
+            $picture = $this->input->post('picture');
+
+            $resp = $this->user_model->edit_user($id, $name, $email, $phone, $picture);
+            if ($resp) {
+                redirect('test/users');
+            } else {
+                $this->load->view('user_edit');
+            }
+                         
+        }
+    }
+
+    public function delete_user($id)
+    {
+        $resp = $this->user_model->delete_user($id);
+        if ($resp) {
+            redirect('test/users');
+        } else {
+            $this->load->view('user_delete');
+        }
+    }
+    
+}
